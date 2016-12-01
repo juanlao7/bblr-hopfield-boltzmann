bfa14a9393f7c741e68e7d19bb821c47eb3b4e1a
MrJavo94 <javierbeltranj@gmail.com>
2016-12-01 12:12:34 +0100

    Added SimpleVectorGenerator for an easier testing

---
+++ src/bblr/generators/SimpleVectorGenerator.py
@@ -0,0 +1,27 @@
+import numpy as np
+from random import randint
+
+def genRandomBinaryVector(n):
+    '''
+    Generates a vector of -1, 1 in a random pattern
+    n: length of the vector
+    '''
+    return np.array([randint(0,1) for _ in range(1,n+1)]) * 2 - 1
+
+def genAlternatingVector(n):
+    '''
+    Generates a vector of -1, 1 alternated
+    n: length of the vector
+    '''
+    a = np.empty((n,))
+    a[::2] = 1
+    a[1::2] = -1
+    return a
+
+def genRepeatedVector(val, n):
+    '''
+    Generates a vector with just one value repeated
+    val: the value of each element
+    n: length of the vector
+    '''
+    return np.repeat(val, n)
