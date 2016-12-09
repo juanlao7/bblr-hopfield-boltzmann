84270eb63281a9ce55b24bf3940b2d2bd971dd81
gbg141 <guille_gbg@hotmail.com>
2016-12-09 20:50:28 +0100

    Hpfield Class modified, now the method recall is asynchronous
    However, the Storkey learning rule seems to behave badly as soon as the
    samples are correlated, even if it is a very small correlation. This id
    not the expected behavior, so it must be checked (I have tried to
    resolve it, without any luck).

--- src/bblr/sandbox/HopfieldExample.py
+++ src/bblr/sandbox/HopfieldExample.py
@@ -1,32 +1,102 @@
 import numpy as np
 from bblr.models import Hopfield
-from bblr.generators import SimpleLetterGenerator as gen
 
 
-partialA = """
+partialA1 = """
 X...X
 .XX..
 .....
 .XXX.
 .XXX.
 """
-
-completeA = """
-.XXX.
-X...X
+partialA2 = """
+.XXXX
+X..XX
 XXXXX
 X...X
-X...X
+XX..X
 """
+
+
+I = """
+..X..
+..X..
+..X..
+..X..
+..X..
+"""
+
+partialI = """
+.X...
+..X..
+.X...
+..X..
+...X.
+"""
+
+partialZ = """
+.XXXX
+...X.
+X....
+.X...
+XX.XX
+"""
+
+U = """
+X...X
+X...X
+X...X
+X...X
+XXXXX
+"""
+
+
+def exampleWithLetters():
+    from bblr.generators import SimpleLetterGenerator as gen
     
-a = gen.to_pattern(gen.A)
-z = gen.to_pattern(gen.Z)
-patterns = np.atleast_2d((a,z))
-print patterns
-hop = Hopfield.Hopfield(verbose=True)
-'''Using Hebbian rule'''
-#hop.trainHebbian(patterns, normW=True)
-'''Using Storkey rule'''
-hop.trainStorkey(patterns, normW=True)
-recovered = hop.recall(gen.to_pattern(partialA),steps=10)
-gen.display(recovered)
+    a = gen.to_pattern(gen.A)
+    z = gen.to_pattern(gen.Z)
+    i = gen.to_pattern(I)
+    u = gen.to_pattern(U)
+    partialz = gen.to_pattern(partialZ)
+    partiali = gen.to_pattern(partialI)
+    partiala2 = gen.to_pattern(partialA2)
+    partiala1 = gen.to_pattern(partialA1)
+    
+    test = a
+    
+    hop = Hopfield.Hopfield(verbose=True)
+    patterns = np.atleast_2d((i, u, z, a))
+    '''Using Storkey rule'''
+    hop.train(patterns, learningRule='storkey')
+    '''Using Hebbian rule'''
+    #hop.train(patterns)
+    recovered = hop.recall(test)
+    #recovered = hop.recall(i)
+    print 'Patterns', patterns
+    print 'Test patterns:\n', test
+    print "Recovered\n", recovered
+    gen.display(test)
+    gen.display(recovered)
+
+
+def exampleWithVectors(vector_size=25):
+    from bblr.generators import SimpleVectorGenerator as gen
+    
+    patterns = np.vstack((gen.genAlternatingVector(vector_size), \
+                         gen.genRepeatedVector(1, vector_size)))
+    test = gen.genRandomBinaryVector(vector_size)
+    test1 = gen.genRepeatedVector(1, vector_size)
+    
+    hop = Hopfield.Hopfield(verbose=True)
+    hop.train(patterns, learningRule='storkey')
+    recovered = hop.recall(test, maxSteps=11)
+    print 'Training patterns:', patterns
+    print 'Test patterns:', test
+    print 'Recovered:', np.around(recovered)
+    
+'''
+Main
+'''
+#exampleWithVectors()
+exampleWithLetters()
